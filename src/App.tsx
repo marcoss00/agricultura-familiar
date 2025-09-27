74502b7bb84adf29780a5dfda2a2d4c099ad6d78
marcoss00 <marcossvinc@gmail.com>
2025-09-27 20:06:41 -0300

    ajuste para funcionar no github pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/agricultura-familiar">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
